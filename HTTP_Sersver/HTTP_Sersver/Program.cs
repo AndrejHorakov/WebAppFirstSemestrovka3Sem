55cac963d5a58b02f982bd47ce0491b050c134e1
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2022-10-29 18:58:47 +0300

    HW for 29.10.22

---
+++ HTTP_Sersver/HTTP_Sersver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new HttpServer().Start();
+        }
+    }
+}
